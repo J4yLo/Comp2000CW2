75f616482e362448e2310aa5164e5f78747d2508
J4yLo <jason.george@students.plymouth.ac.uk>
2022-01-26 14:21:54 +0000

    Update a project and Delete a project page implemented

---
+++ app/src/main/java/com/example/comp2000cw2/Remove_Program.java
@@ -0,0 +1,14 @@
+package com.example.comp2000cw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Remove_Program extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_program);
+    }
+}
---
+++ app/src/main/java/com/example/comp2000cw2/Update_Project.java
@@ -0,0 +1,14 @@
+package com.example.comp2000cw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Update_Project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_project);
+    }
+}
